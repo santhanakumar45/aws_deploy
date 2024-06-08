6243e63c8434afaeee3b36779885d8d6ce74f516
santhanakumar45 <santhanakumar@zealzoft.com>
2024-06-08 09:48:40 +0530

    Your commit message

---
+++ app.js
@@ -0,0 +1,37 @@
+import express, { urlencoded } from "express";
+const app = express()
+
+
+app.use(express.json())
+
+// const conn = new Client({
+//   user: "postgres",
+//   password: "12345678",
+//   database: "postgres",
+//   port: 5432,
+//   host: "database-2.ctqys2wi0b5w.us-east-1.rds.amazonaws.com",
+//   ssl:{
+//     rejectUnauthorized:false,
+//   }
+
+app.get("/",(req,res)=>{
+  console.log("Welcome")
+})
+// });
+
+// const conn = new Sequelize(
+//     `postgres://postgres:12345678@database-2.ctqys2wi0b5w.us-east-1.rds.amazonaws.com:5432/postgres`,
+//     { dialect: "postgres",  logging: false  }
+//   );
+
+// conn.connect()
+//   .then(() => {
+//     console.log('Connected to PostgreSQL database successfully.');
+//     // Perform database operations here
+//   })
+//   .catch(err => {
+//     console.error('Error connecting to PostgreSQL database:', err);
+//   });
+
+  
+app.listen(3000,()=>console.log("server is running 3000"));
